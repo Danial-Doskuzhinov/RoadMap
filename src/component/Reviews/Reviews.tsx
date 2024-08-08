55d49dd0270cb973abd1e99be43d7d01fc3a90a6
Danial-Doskuzhinov <doskuzhinovdanial@gmail.com>
2024-08-08 16:58:17 +0500

    added component Reviews

--- src/component/Reviews/Reviews.tsx
+++ src/component/Reviews/Reviews.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import { MainContainer2 ,User1 } from '../../assets/index';
 
 const testimonials = [
   {
     name: 'Алия Ансат',
     title: 'предприниматель',
-    image: 'path_to_image', 
+    image: User1, 
     text: 'Спасибо за тест и рекомендации. Я долгое время не могла сама это всё четко отследить и в одну систему собрать и сформулировать. Очень детальный и точный анализ на определение профориентации, я в последние несколько лет стараюсь разобраться в себе, и теперь есть четкое понимание и структурность, все гениальное - просто!',
   },
   {
@@ -41,30 +42,36 @@ const Testimonials = () => {
       setCurrent((current) => current+ 1);
     }
   };
-
+  const progressPercentage = ((current + 1) / testimonials.length) * 100;
   const handlePrev = () => {
     if (current > 0) {
       setCurrent((current) =>current - 1);
     }
   }
   return (
-    <div className="p-6">
+    <>
+    <div className="mx-[128px] mt-[280px] mb-[128px] ">
+        <section className=''>
       <h2 className="text-2xl font-bold text-office-brown-700 mb-2">Отзывы пользователей</h2>
-      <p className="text-base text-gray-500 mb-8">Узнайте, как наше приложение помогло другим пользователям достичь своих целей. Истории успеха, отзывы и рекомендации</p>
-      <div className="flex items-start mb-4">
+      <p className="text-base text-[#645D5D] w-[640px] mb-8">Узнайте, как наше приложение помогло другим пользователям достичь своих целей. Истории успеха, отзывы и рекомендации</p>
+      </section>
+      <div className="flex justify-between">
+        <section>
         <img
           src={testimonials[current].image}
           alt={testimonials[current].name}
           className="w-16 h-16 rounded-full mr-4"
         />
-        <div>
           <p className="text-lg font-bold text-gray-700">{testimonials[current].name}</p>
           <p className="text-sm text-gray-500 mb-2">{testimonials[current].title}</p>
-          <p className="text-base font-normal leading-[22.4px] text-gray-700">{testimonials[current].text}</p>
+          </section>
+          <p className="text-[24px] w-[933px] font-normal leading-[22.4px] text-gray-700">{testimonials[current].text}</p>
         </div>
+       
+      <div className="flex mb-12 items-center justify-between mt-6">
+      <div className="w-full h-1 bg-gray-200 rounded-full  mr-5">
+        <div className="h-1 bg-blue-500 rounded-full transition-all duration-300" style={{ width: `${progressPercentage}%` }}></div>
       </div>
-      <div className="flex items-center justify-between mt-6">
-        <button className="px-4 py-2 bg-blue-100 text-blue-600 rounded-full">Написать отзыв</button>
         <div className="flex space-x-2">
           <button 
             onClick={handlePrev} 
@@ -77,8 +84,15 @@ const Testimonials = () => {
             <span>&gt;</span>
           </button>
         </div>
+       
       </div>
+      <button className="px-4 py-2 bg-blue-600 text-blue-100 rounded-full">Написать отзыв</button>
+
     </div>
+    <div className='bg-[#FFD14C]'>
+    <img className='' src={MainContainer2} alt="О нас " />
+    </div>
+    </>
   );
 }
 
